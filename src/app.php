16027400b1c36d8aaf9692511cd924ff2653def7
Varun <varunsridharan23@gmail.com>
2020-11-01 17:43:27 +0530

    Fixed !Fatal error: Uncaught Error: Call to undefined function gh_env() in /action-repository-roster/app.php:4

--- src/app.php
+++ src/app.php
@@ -1,11 +1,14 @@
 <?php
-global $repo_info, $repo;
-define( 'APP_PATH', __DIR__ . '/' );
-define( 'WORK_DIR', gh_env( 'GITHUB_WORKSPACE' ) );
-$repos = array( get_env( 'GITHUB_REPOSITORY' ) );
-
-require_once APP_PATH . '../../actions-toolkit/toolkit/gh-api.php';
+require_once '/gh-toolkit/php.php';
 require_once APP_PATH . 'functions.php';
+
+global $repo_info, $repo;
+
+define( 'APP_PATH', __DIR__ . '/' );
+
+define( 'WORK_DIR', gh_env( 'GITHUB_WORKSPACE' ) );
+
+$repos = array( get_env( 'GITHUB_REPOSITORY' ) );
 
 foreach ( $repos as $repo ) {
 	$repo_info = $github_api->decode( $github_api->get( sprintf( 'repos/%s', $repo ) ) );
