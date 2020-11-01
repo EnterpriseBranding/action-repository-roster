ea1590ad6d64f0b554b8a78ec0f66697d888c81c
Varun <varunsridharan23@gmail.com>
2020-11-01 19:00:06 +0530

    Updated

--- src/forks.php
+++ src/forks.php
@@ -5,84 +5,85 @@ $api_fork_per_page = 100;
 $forks             = gh_input( 'FORK', 'README.md' );
 
 if ( false !== $forks ) {
-	$forks            = ( true === $forks ) ? 'README.md' : $forks;
-	$fork_output_type = gh_input( 'FORK_OUTPUT_TYPE', 'markdown' );
-	$fork_show_count  = gh_input( 'FORK_COUNTS', 7 );
-	$fork_description = gh_input( 'FORK_DESCRIPTION', '' );
+	$forks             = ( true === $forks ) ? 'README.md' : $forks;
+	$fork_output_type  = gh_input( 'FORK_OUTPUT_TYPE', 'markdown' );
+	$fork_show_count   = gh_input( 'FORK_COUNTS', 7 );
+	$fork_description  = gh_input( 'FORK_DESCRIPTION', '' );
 	$total_forks_count = ( isset( $repo_info->forks_count ) && ! empty( $repo_info->forks_count ) ) ? $repo_info->forks_count : '';
 
 	if ( empty( $total_forks_count ) ) {
-		return;
-	}
+		$html = '';
+	} else {
+		function fetch_recent_forks( $repo, $limit = 10, $page = '1' ) {
+			global $github_api, $api_fork_per_page;
+			$data     = $github_api->decode( $github_api->get( sprintf( 'repos/%s/forks?sort=newest&per_page=' . $api_fork_per_page . '&page=' . $page, $repo ) ) );
+			$response = array();
+			foreach ( $data as $fork ) {
+				if ( ! isset( $fork->owner ) ) {
+					continue;
+				}
 
-	function fetch_recent_forks( $repo, $limit = 10, $page = '1' ) {
-		global $github_api, $api_fork_per_page;
-		$data     = $github_api->decode( $github_api->get( sprintf( 'repos/%s/forks?sort=newest&per_page=' . $api_fork_per_page . '&page=' . $page, $repo ) ) );
-		$response = array();
-		foreach ( $data as $fork ) {
-			if ( ! isset( $fork->owner ) ) {
-				continue;
+				$response[] = array(
+					'owner'      => $fork->owner->login,
+					'avatar_url' => $fork->owner->avatar_url,
+					'html_url'   => $fork->owner->html_url,
+				);
+
+				if ( 0 !== $response && count( $response ) == $limit ) {
+					break;
+				}
 			}
-
-			$response[] = array(
-				'owner'      => $fork->owner->login,
-				'avatar_url' => $fork->owner->avatar_url,
-				'html_url'   => $fork->owner->html_url,
-			);
-
-			if ( 0 !== $response && count( $response ) == $limit ) {
-				break;
-			}
+			return $response;
 		}
-		return $response;
-	}
 
-	$base_response = fetch_recent_forks( $repo, $fork_show_count );
+		$base_response = fetch_recent_forks( $repo, $fork_show_count );
 
-	if ( count( $base_response ) < $fork_show_count ) {
-		$status = true;
-		$page   = 2;
-		while ( $status ) {
-			$new = fetch_recent_forks( $repo, $fork_show_count, $page++ );
-			if ( empty( $new ) ) {
-				$status = false;
-				break;
-			}
-			foreach ( $new as $owner_info ) {
-				$base_response[] = $owner_info;
+		if ( count( $base_response ) < $fork_show_count ) {
+			$status = true;
+			$page   = 2;
+			while ( $status ) {
+				$new = fetch_recent_forks( $repo, $fork_show_count, $page++ );
+				if ( empty( $new ) ) {
+					$status = false;
+					break;
+				}
+				foreach ( $new as $owner_info ) {
+					$base_response[] = $owner_info;
+
+					if ( count( $base_response ) == $fork_show_count ) {
+						$status = false;
+						break;
+					}
+				}
 
 				if ( count( $base_response ) == $fork_show_count ) {
 					$status = false;
 					break;
 				}
-			}
 
-			if ( count( $base_response ) == $fork_show_count ) {
-				$status = false;
-				break;
-			}
-
-			if ( $page > $total_forks_count || $page === $total_forks_count ) {
-				$status = false;
-				break;
+				if ( $page > $total_forks_count || $page === $total_forks_count ) {
+					$status = false;
+					break;
+				}
 			}
 		}
-	}
 
-	if ( false !== $fork_description ) {
-		if ( empty( $fork_description ) ) {
-			if ( $total_forks_count == $fork_show_count ) {
-				$fork_description = '<i><b>' . $total_forks_count . '</b> people have forked this repository</i>';
-			} elseif ( $fork_show_count < $total_forks_count ) {
-				$fork_description = '<i><b>' . ( $total_forks_count - $fork_show_count ) . '</b> Others have forked this repository</i>';
-			} else {
-				$fork_description = '<i><b>' . ( $total_forks_count - $fork_show_count ) . '</b> Others have forked this repository</i>';
+		if ( false !== $fork_description ) {
+			if ( empty( $fork_description ) ) {
+				if ( $total_forks_count == $fork_show_count ) {
+					$fork_description = '<i><b>' . $total_forks_count . '</b> people have forked this repository</i>';
+				} elseif ( $fork_show_count < $total_forks_count ) {
+					$fork_description = '<i><b>' . ( $total_forks_count - $fork_show_count ) . '</b> Others have forked this repository</i>';
+				} else {
+					$fork_description = '<i><b>' . ( $total_forks_count - $fork_show_count ) . '</b> Others have forked this repository</i>';
+				}
 			}
+			$fork_description = str_replace( '[count]', $total_forks_count, $fork_description );
 		}
-		$fork_description = str_replace( '[count]', $total_forks_count, $fork_description );
+
+		$html = generate_output( $fork_output_type, $base_response, $fork_description );
 	}
 
-	$html = generate_output( $fork_output_type, $base_response, $fork_description );
 	$file = save_output( $fork_output_type, $html, $forks, 'REPOSITORY_FORKS' );
 	shell_exec( 'git add -f ' . $file );
 	shell_exec( 'git commit -m "[Repository Roster] Updated :cyclone: Forkers Information" ' );
--- src/stars.php
+++ src/stars.php
@@ -12,73 +12,73 @@ if ( false !== $stars ) {
 	$total_stars_count = ( isset( $repo_info->stargazers_count ) && ! empty( $repo_info->stargazers_count ) ) ? $repo_info->stargazers_count : '';
 
 	if ( empty( $total_stars_count ) ) {
-		return;
-	}
+		$html = '';
+	} else {
+		$total_stars_page = ceil( $total_stars_count / $api_stars_per_page );
 
-	$total_stars_page = ceil( $total_stars_count / $api_stars_per_page );
+		function fetch_recent_stars( $repo, $limit = 10, $page = '1' ) {
+			global $github_api, $api_stars_per_page;
+			$data     = $github_api->decode( $github_api->get( sprintf( 'repos/%s/stargazers?per_page=' . $api_stars_per_page . '&page=' . $page, $repo ) ) );
+			$response = array();
+			foreach ( $data as $_star ) {
+				if ( ! isset( $_star->login ) ) {
+					continue;
+				}
 
-	function fetch_recent_stars( $repo, $limit = 10, $page = '1' ) {
-		global $github_api, $api_stars_per_page;
-		$data     = $github_api->decode( $github_api->get( sprintf( 'repos/%s/stargazers?per_page=' . $api_stars_per_page . '&page=' . $page, $repo ) ) );
-		$response = array();
-		foreach ( $data as $_star ) {
-			if ( ! isset( $_star->login ) ) {
-				continue;
+				$response[] = array(
+					'owner'      => $_star->login,
+					'avatar_url' => $_star->avatar_url,
+					'html_url'   => $_star->html_url,
+				);
 			}
-
-			$response[] = array(
-				'owner'      => $_star->login,
-				'avatar_url' => $_star->avatar_url,
-				'html_url'   => $_star->html_url,
-			);
+			return $response;
 		}
-		return $response;
-	}
 
-	$status = true;
-	$retun  = array();
-	$page   = $total_stars_page;
-	while ( $status ) {
-		$new = fetch_recent_stars( $repo, $stars_show_count, $page-- );
-		if ( empty( $new ) ) {
-			$status = false;
-			break;
-		}
-		krsort( $new );
-		foreach ( $new as $owner_info ) {
-			$retun[] = $owner_info;
+		$status = true;
+		$retun  = array();
+		$page   = $total_stars_page;
+		while ( $status ) {
+			$new = fetch_recent_stars( $repo, $stars_show_count, $page-- );
+			if ( empty( $new ) ) {
+				$status = false;
+				break;
+			}
+			krsort( $new );
+			foreach ( $new as $owner_info ) {
+				$retun[] = $owner_info;
+
+				if ( count( $retun ) == $stars_show_count ) {
+					$status = false;
+					break;
+				}
+			}
 
 			if ( count( $retun ) == $stars_show_count ) {
 				$status = false;
 				break;
 			}
-		}
 
-		if ( count( $retun ) == $stars_show_count ) {
-			$status = false;
-			break;
-		}
-
-		if ( $page < 0 || $page === 0 ) {
-			$status = false;
-			break;
-		}
-	}
-
-	if ( false !== $stars_description ) {
-		if ( empty( $stars_description ) ) {
-			if ( $total_stars_count == $stars_show_count ) {
-				$stars_description = '<i><b>' . $total_stars_count . '</b> people have starred this repository</i>';
-			} elseif ( $stars_show_count < $total_stars_count ) {
-				$stars_description = '<i><b>' . ( $total_stars_count - $stars_show_count ) . '</b> Others have starred this repository</i>';
-			} else {
-				$stars_description = '<i><b>' . ( $total_stars_count - $stars_show_count ) . '</b> Others have starred this repository</i>';
+			if ( $page < 0 || $page === 0 ) {
+				$status = false;
+				break;
 			}
 		}
-		$stars_description = str_replace( '[count]', $total_stars_count, $stars_description );
-	}
 
-	$html = generate_output( $stars_output_type, $retun, $stars_description );
+		if ( false !== $stars_description ) {
+			if ( empty( $stars_description ) ) {
+				if ( $total_stars_count == $stars_show_count ) {
+					$stars_description = '<i><b>' . $total_stars_count . '</b> people have starred this repository</i>';
+				} elseif ( $stars_show_count < $total_stars_count ) {
+					$stars_description = '<i><b>' . ( $total_stars_count - $stars_show_count ) . '</b> Others have starred this repository</i>';
+				} else {
+					$stars_description = '<i><b>' . ( $total_stars_count - $stars_show_count ) . '</b> Others have starred this repository</i>';
+				}
+			}
+			$stars_description = str_replace( '[count]', $total_stars_count, $stars_description );
+		}
+
+		$html = generate_output( $stars_output_type, $retun, $stars_description );
+	}
 	$file = save_output( $stars_output_type, $html, $stars, 'REPOSITORY_STARS' );
 	shell_exec( 'git add -f ' . $file );
 	shell_exec( 'git commit -m "[Repository Roster] Updated :star2: Stargazers Information" ' );
