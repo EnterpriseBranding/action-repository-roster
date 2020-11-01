5943684190afd687a9ea6c73a987c1d8e95142c7
Varun <varunsridharan23@gmail.com>
2020-11-01 18:21:04 +0530

    Updated

--- src/app.php
+++ src/app.php
@@ -13,9 +13,14 @@ $repos = array( gh_env( 'GITHUB_REPOSITORY' ) );
 require_once APP_PATH . 'functions.php';
 
 foreach ( $repos as $repo ) {
-	$repo_info = $github_api->decode( $github_api->get( 'repos/' . $repo ) );
-	require_once APP_PATH . 'forks.php';
-	require_once APP_PATH . 'stars.php';
+	try {
+		$repo_info = $github_api->decode( $github_api->get( 'repos/' . $repo ) );
+		require_once APP_PATH . 'forks.php';
+		require_once APP_PATH . 'stars.php';
+	} catch ( \Milo\Github\RateLimitExceedException $exception ) {
+		print_r( $exception );
+	}
+
 }
 
 
