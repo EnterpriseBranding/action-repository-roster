ec98a15daf4198fb9aa0522e7d9bd8c6d68e57a6
Varun <varunsridharan23@gmail.com>
2020-11-01 17:44:28 +0530

    Fixed DIR Warning !

--- src/app.php
+++ src/app.php
@@ -1,6 +1,5 @@
 <?php
 require_once '/gh-toolkit/php.php';
-require_once APP_PATH . 'functions.php';
 
 global $repo_info, $repo;
 
@@ -9,6 +8,9 @@ define( 'APP_PATH', __DIR__ . '/' );
 define( 'WORK_DIR', gh_env( 'GITHUB_WORKSPACE' ) );
 
 $repos = array( get_env( 'GITHUB_REPOSITORY' ) );
+
+
+require_once APP_PATH . 'functions.php';
 
 foreach ( $repos as $repo ) {
 	$repo_info = $github_api->decode( $github_api->get( sprintf( 'repos/%s', $repo ) ) );
