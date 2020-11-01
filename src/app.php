c1cf358406166444c085a35ec01465d82c4b0a54
Varun <varunsridharan23@gmail.com>
2020-11-01 17:59:03 +0530

    Updated

--- src/app.php
+++ src/app.php
@@ -14,9 +14,10 @@ $repos = array( gh_env( 'GITHUB_REPOSITORY' ) );
 require_once APP_PATH . 'functions.php';
 
 foreach ( $repos as $repo ) {
-	var_dump( $repo );
+	$repo_info = $github_api->decode( $github_api->get( sprintf( 'repos/%s', $repo ) ) );
+
+	var_dump( $repo_info );
 	exit;
-	#$repo_info = $github_api->decode( $github_api->get( sprintf( 'repos/%s', $repo ) ) );
 	#require_once APP_PATH . 'forks.php';
 	#require_once APP_PATH . 'stars.php';
 }
