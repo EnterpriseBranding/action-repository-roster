4911d61945e0e7347a55eaac1d9862465a1f7ba6
Varun <varunsridharan23@gmail.com>
2020-11-02 10:34:52 +0530

    Updated.

--- src/app.php
+++ src/app.php
@@ -5,21 +5,15 @@ require_once '/gh-toolkit/gh-api.php';
 global $repo_info, $repo;
 
 define( 'APP_PATH', __DIR__ . '/' );
-
-define( 'WORK_DIR', gh_env( 'GITHUB_WORKSPACE' ) . '/' );
-
-$repos = array( gh_env( 'GITHUB_REPOSITORY' ) );
+define( 'IMAGE_SAVE_DIR', gh_input( 'IMAGE_SAVE_PATH', '.github/roster/' ) );
 
 require_once APP_PATH . 'functions.php';
 
-foreach ( $repos as $repo ) {
-	try {
-		$repo_info = $github_api->decode( $github_api->get( 'repos/' . $repo ) );
-		require_once APP_PATH . 'forks.php';
-		require_once APP_PATH . 'stars.php';
-	} catch ( \Milo\Github\RateLimitExceedException $exception ) {
-		print_r( $exception );
-	}
+try {
+	$repo      = gh_env( 'GITHUB_REPOSITORY' );
+	$repo_info = $github_api->decode( $github_api->get( 'repos/' . $repo ) );
+	require_once APP_PATH . 'forks.php';
+	require_once APP_PATH . 'stars.php';
+} catch ( \Milo\Github\RateLimitExceedException $exception ) {
+	print_r( $exception );
 }
-
-
