8bdc1dbb4d79d49dc887ebb1ce8b8b69306de07d
Varun <varunsridharan23@gmail.com>
2020-11-02 08:03:12 +0530

    Added List.

--- src/output/markdown.php
+++ src/output/markdown.php
@@ -1,6 +1,10 @@
 <?php
 function generate_markdown_output( $data, $style, $description ) {
 	if ( false !== strpos( $style, 'table' ) ) {
-		echo generate_markdown_table( $data, $description, $style );
+		return generate_markdown_table( $data, $description, $style );
+	}
+
+	if ( false !== strpos( $style, 'list' ) ) {
+		echo generate_markdown_list( $data, $description, $style );
 	}
 }
