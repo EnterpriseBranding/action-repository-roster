4ea2b5cce48aa6416ddb655baac34174096ed52a
Varun <varunsridharan23@gmail.com>
2020-11-02 16:59:28 +0530

    Updated

--- src/app.php
+++ src/app.php
@@ -12,11 +12,17 @@ require_once APP_PATH . 'functions.php';
 try {
 	$repo      = gh_env( 'GITHUB_REPOSITORY' );
 	$repo_info = $github_api->decode( $github_api->get( 'repos/' . $repo ) );
-	gh_log();
+	gh_log( 'Repository Forks' );
+	gh_log( str_repeat( '-', 30 ) );
 	require_once APP_PATH . 'forks.php';
+	gh_log( str_repeat( '-', 30 ) );
+
 	gh_log();
-	gh_log();
+
+	gh_log( 'Repository Stargazers' );
+	gh_log( str_repeat( '-', 30 ) );
 	require_once APP_PATH . 'stars.php';
+	gh_log( str_repeat( '-', 30 ) );
 	gh_log();
 } catch ( \Milo\Github\RateLimitExceedException $exception ) {
 	print_r( $exception );
